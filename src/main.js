cf72f5ce2c49b233a966b842c79efcfec674806f
hmAAmh <DatFido@yahoo.com>
2022-04-17 09:39:29 -0700

    Added comments

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-console.log("different now");
+/*
+    Sophie Martin
+    Rocket Patrol Mod
+    4/17/22
+    Took roughly 3 days to complete
+    Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (60)
+    Add your own (copyright-free) background music to the Play scene (5)
+    Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (20)
+    Create a new animated sprite for the Spaceship enemies (10)
+    Implement the speed increase that happens after 30 seconds in the original game (5)
+*/
 
 let config = {
     type: Phaser.CANVAS,
